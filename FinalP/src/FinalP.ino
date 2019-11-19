fa0df529afad060782de9a486a087c554a72a605
Lindsay Gillespie <42749226+LinGill21@users.noreply.github.com>
2019-11-18 20:30:37 -0600

    fixed to current version

--- FinalP/src/FinalP.ino
+++ FinalP/src/FinalP.ino
@@ -8,16 +8,18 @@ const  int nutrual =0.075*255;
 const int min =0.05 *255;
 const int max = 0.1 * 255;
 
+int servoAction(String message){
+analogWrite(D0,minturn,50);
+if (Time.now()>(waitingtime+60)){
+  analogWrite(D0,nutrual,50);
+  waitingtime=Time.now();
+}  
+}
+
 void setup() {
   Serial.begin(9600);
+  Particle.function("translator",servoAction);
   pinMode(D0,OUTPUT);
-  pinMode(A0,INPUT_PULLUP);
-  analogWrite(D0,min,50);
-  delay(500);
-  analogWrite(D0,nutrual,50);
-  delay(500);
-  analogWrite(D0,max,50);
-  delay (500);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
