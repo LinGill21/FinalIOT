a714054771c5514a7435aee787a2ec880e2b5bcb
MistyATG <53179893+MistyATG@users.noreply.github.com>
2019-11-18 21:03:35 -0600

    Update FinalP.ino
    so it allow me to ciommit and the code i think should work bu needs to checked on photon

--- FinalP/src/FinalP.ino
+++ FinalP/src/FinalP.ino
@@ -7,6 +7,8 @@
 const  int nutrual =0.075*255;
 const int min =0.05 *255;
 const int max = 0.1 * 255;
+int tempSensor = A0;
+double temperatureC = 0.0;
 
 int servoAction(String message){
 analogWrite(D0,minturn,50);
@@ -18,6 +20,8 @@ if (Time.now()>(waitingtime+60)){
 
 void setup() {
   Serial.begin(9600);
+  pinMode(tempSensor, AN_INPUT);
+ 
   Particle.function("translator",servoAction);
   pinMode(D0,OUTPUT);
 }
@@ -25,6 +29,9 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
 
+double voltage = analogRead(tempSensor)/4095.0*3.3;
+temperatureC = 104.6*voltage - 54.4;
+  
 analogWrite(D0,90,50);
 
 }
